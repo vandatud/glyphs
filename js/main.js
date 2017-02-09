182e84b8397d879e47b9121979a2d2d72453217c
Mahog <s0020279@mail.zih.tu-dresden.de>
2017-02-09 15:11:57 +0100

    add: update repo to current state

---
+++ js/main.js
@@ -0,0 +1,181 @@
+var margin = {
+  top: 36,
+  right: 50,
+  bottom: 20,
+  left: 50
+};
+
+var width = 240 - margin.left - margin.right;
+var height = 240 - margin.top - margin.bottom;
+var labelMargin = 8;
+
+var scale = d3.scaleLinear()
+  .domain([0, 4])
+  .range([0, 100])
+
+// for specifics to whiskies.csv see documentation in drawFlowerplot()
+function drawStarplot(noOfRows) {
+  d3.csv('whiskies.csv')
+    .row(function(d) {
+        d.Body = +d.Body;
+        d.Sweetness = +d.Sweetness;
+        d.Smoky = +d.Smoky;
+        d.Medicinal = +d.Medicinal;
+        d.Tobacco = +d.Tobacco;
+        d.Honey = +d.Honey;
+        d.Spicy = +d.Spicy;
+        d.Winey = +d.Winey;
+        d.Nutty = +d.Nutty;
+        d.Malty = +d.Malty;
+        d.Fruity = +d.Fruity;
+        d.Floral = +d.Floral;
+        return d;
+    })
+    .get(function(error, rows) {
+      let labels = [];
+      let accessors = [];
+      let pp = 0;
+      for (let x in rows[0]) {
+        pp++;
+        if (pp > 3 + noOfRows || pp > 14) break;
+        if (pp < 3) continue;
+        labels.push(x);
+        accessors.push(function(d) { return scale(d[x]); })
+      }
+
+      var star = d3.starPlot()
+        .width(width)
+        .accessors(accessors)
+        .labels(labels)
+        .includeLabels(showStarLabels)
+        .title(function(d) { return d.Distillery; })
+        .margin(margin)
+        .labelMargin(labelMargin)
+
+      rows.forEach(function(d, i) {
+
+        if (i > 11) return; // draw 4 starplots (4 rows of data)
+
+        d3.select('#target').append('svg')
+          .attr('class', 'chart')
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', width + margin.top + margin.bottom)
+          .append('g')
+            .datum(d)
+            .call(star)
+      });
+    });
+    $('#starRowsCounter').text("Achsen: " + (currentNoOfRowsFlower+1));
+}
+
+function drawFlowerplot(noOfRows) {
+  d3.csv('whiskies.csv')
+    .row(function(d) { // all values are set to their absolutes
+        d.Body = +d.Body;
+        d.Sweetness = +d.Sweetness;
+        d.Smoky = +d.Smoky;
+        d.Medicinal = +d.Medicinal;
+        d.Tobacco = +d.Tobacco;
+        d.Honey = +d.Honey;
+        d.Spicy = +d.Spicy;
+        d.Winey = +d.Winey;
+        d.Nutty = +d.Nutty;
+        d.Malty = +d.Malty;
+        d.Fruity = +d.Fruity;
+        d.Floral = +d.Floral;
+        return d;
+    })
+    .get(function(error, rows) {
+      let labels = [];
+      let accessors = [];
+      let pp = 0;
+
+      // get headers of the data-columns
+      for (let x in rows[0]) {
+        pp++;
+        // in the whiskies.csv, the first three rows and some of the last ones
+        // are not suitable for plotting, therefore ignore those
+        if (pp > 3 + noOfRows || pp > 14) break;
+        if (pp < 3) continue;
+        labels.push(x); // name of the column
+        // a linear scale for the current data entry
+        accessors.push(function(d) { return scale(d[x]); })
+      }
+
+      // setup the flower's attributes
+      var flower = d3.flowerPlot()
+        .width(width)
+        .accessors(accessors)
+        .labels(labels)
+        .includeLabels(showFlowerLabels)
+        .title(function(d) { return d.Distillery; }) // unique to whiskies.csv
+        .margin(margin)
+        .labelMargin(labelMargin)
+
+      rows.forEach(function(d, i) {
+
+        if (i > 11) return;
+
+        // draw the flower
+        d3.select('#flowerTarget').append('svg')
+          .attr('class', 'chart')
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', width + margin.top + margin.bottom)
+          .append('g')
+            .datum(d)
+            .call(flower)
+      });
+    });
+    $('#flowerRowsCounter').text("Achsen: " + (currentNoOfRowsFlower+1));
+}
+
+let currentNoOfRowsStar = 5;
+let currentNoOfRowsFlower = 5;
+
+let showFlowerLabels = true;
+let showStarLabels = true;
+
+drawStarplot(currentNoOfRowsStar);
+drawFlowerplot(currentNoOfRowsFlower);
+
+$('#guidelineToggle').on('click', function() { $('.star-axis').toggle(); });
+$('#flowerGuidelineToggle').on('click', function() { $('.flower-axis').toggle(); });
+
+$('#flowerLabelsToggle').on('click', function() {
+  // showFlowerLabels = !showFlowerLabels;
+  $('#flowerTarget svg text').toggle();
+});
+
+$('#starLabelsToggle').on('click', function() {
+  // showStarLabels = !showStarLabels;
+  $('#target svg text').toggle();
+});
+
+$('#moreDataButton').on('click', function() {
+  if (currentNoOfRowsStar > 14) return;
+  currentNoOfRowsStar++;
+
+  $('#target svg').remove();
+  drawStarplot(currentNoOfRowsStar)
+});
+$('#lessDataButton').on('click', function() {
+  if (currentNoOfRowsStar < 4) return;
+  currentNoOfRowsStar--;
+
+  $('#target svg').remove();
+  drawStarplot(currentNoOfRowsStar)
+});
+$('#flowerMoreDataButton').on('click', function() {
+  if (currentNoOfRowsFlower > 14) return;
+  currentNoOfRowsFlower++;
+
+  $('#flowerTarget svg').remove();
+  drawFlowerplot(currentNoOfRowsFlower)
+});
+$('#flowerLessDataButton').on('click', function() {
+  if (currentNoOfRowsFlower < 4) return;
+  currentNoOfRowsFlower--;
+
+  $('#flowerTarget svg').remove();
+  drawFlowerplot(currentNoOfRowsFlower)
+});
---
+++ js/starplot.js
@@ -0,0 +1,184 @@
+d3.starPlot = function() {
+  let width = 200;
+  let labelMargin = 20;
+  let includeGuidelines = true;
+  let includeLabels = true;
+  let accessors = [];
+  let labels = [];
+  let title = nop;
+
+  let margin = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+  };
+
+  let g;
+  let datum;
+
+  let radius = width / 2;
+  let origin = [radius, radius];
+  let radii = accessors.length;
+  let radians = 2 * Math.PI / radii;
+
+  let scale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, radius]);
+
+  function chart(selection) {
+    datum = selection.datum();
+    g = selection
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    if (includeGuidelines) {
+      drawGuidelines();
+    }
+    if (includeLabels) {
+      drawLabels();
+    }
+
+    drawChart();
+  }
+
+  function drawGuidelines() {
+    let r = 0;
+
+    accessors.forEach(function(d, i) {
+      let l, x, y;
+
+      l = radius;
+      x = l * Math.cos(r);
+      y = l * Math.sin(r);
+
+      g.append('line')
+        .attr('class', 'star-axis')
+        .attr('x1', origin[0])
+        .attr('y1', origin[1])
+        .attr('x2', origin[0] + x)
+        .attr('y2', origin[1] + y)
+
+      r += radians;
+    })
+  }
+
+  function drawLabels() {
+    let r = 0;
+
+    accessors.forEach(function(d, i) {
+      let l, x, y;
+
+      l = radius;
+      x = (l + labelMargin) * Math.cos(r);
+      y = (l + labelMargin) * Math.sin(r);
+
+      g.append('text')
+        .attr('class', 'label')
+        .attr('x', origin[0] + x)
+        .attr('y', origin[1] + y)
+        .text(labels[i])
+        .style('text-anchor', 'middle')
+        .style('dominant-baseline', 'central')
+
+      r += radians;
+    })
+  }
+
+  function drawChart() {
+    g.append('circle')
+      .attr('class', 'origin')
+      .attr('cx', origin[0])
+      .attr('cy', origin[1])
+      .attr('r', 2);
+
+    let path = d3.radialLine();
+
+    let pathData = [];
+    let r = Math.PI / 2;
+    let c = parseInt(datum.RowID) % 50;
+
+    pallette = '1f77b4aec7e8ff7f0effbb782ca02c98df8ad62728ff98969467bdc5b0d58c564bc49c94e377c2f7b6d27f7f7fc7c7c7bcbd22dbdb8d17becf9edae5';
+
+    accessors.forEach(function(d) {
+      pathData.push([
+        r,
+        scale(d(datum))
+      ]);
+
+      r += radians;
+    });
+
+    g.append('path')
+      .attr('class', 'star-path')
+      .attr('transform', 'translate(' + origin[0] + ',' + origin[1] + ')')
+      .attr('fill', '#' + pallette.substring(c, c+6))
+      .attr('d', path(pathData) + 'Z');
+
+    g.append('text')
+      .attr('class', 'title')
+      .attr('x', origin[0])
+      .attr('y', -(margin.top / 2))
+      .text(title(datum))
+      .style('text-anchor', 'middle');
+  }
+
+  function nop() {
+    return;
+  }
+
+  chart.accessors = function(_) {
+    if (!arguments.length) return accessors;
+    accessors = _;
+    radii = accessors.length;
+    radians = 2 * Math.PI / radii;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    radius = width / 2;
+    origin = [radius, radius];
+    scale.range([0, radius])
+    return chart;
+  };
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    origin = [radius, radius];
+    return chart;
+  };
+
+  chart.labelMargin = function(_) {
+    if (!arguments.length) return labelMargin;
+    labelMargin = _;
+    return chart;
+  };
+
+  chart.title = function(_) {
+    if (!arguments.length) return title;
+    title = _;
+    return chart;
+  };
+
+  chart.labels = function(_) {
+    if (!arguments.length) return labels;
+    labels = _;
+    return chart;
+  };
+
+  chart.includeGuidelines = function(_) {
+    if (!arguments.length) return includeGuidelines;
+    includeGuidelines = _;
+    return chart;
+  };
+
+  chart.includeLabels = function(_) {
+    if (!arguments.length) return includeLabels;
+    includeLabels = _;
+    return chart;
+  };
+
+  return chart;
+}
